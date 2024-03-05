64dc48ec1e9a4ff4cacb198af216e56fd184826d
SebasOspinaM <sebastianospinamadrigal@gmail.com>
2024-03-05 15:04:52 -0500

    Avance en clase Main y creación de la super y las sub clases.

---
+++ Punto4/Punto4/Editorial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Punto4;
+class Editorial
+{
+    static void Main(string[] args)
+    {
+        int opc;
+
+
+        do
+        {
+            Console.WriteLine("Ingrese 1 para los datos de un periodico");
+            Console.WriteLine("Ingrese 2 para los datos de un libro");
+            Console.WriteLine("Ingrese 0 para salir");
+            opc = Convert.ToInt32(Console.ReadLine());
+            switch (opc)
+            {
+                case 1:
+                    int numHoja, valHoja, valHojaCol;
+                    string titulo, nomPeriodico, fechaPubli;
+
+                    Console.Write("\nIngrese el número de páginas del periodico: ");
+                    numHoja = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Ingrese el valor de las paginas en blanco y negro: ");
+                    valHoja = Convert.ToInt32(Console.ReadLine());
+                    valHojaCol = valHoja + 200;
+                    Console.Write("Ingrese el título del periodico: ");
+                    titulo = Console.ReadLine();
+                    Console.Write("Ingrese el nombre del periodico: ");
+                    nomPeriodico = Console.ReadLine();
+                    Console.Write("Ingrese la fecha de publicacion (DD/MM/AA): ");
+                    fechaPubli = Console.ReadLine(); 
+
+                    Periodico p = new Periodico(numHoja, valHoja, valHojaCol, titulo, fechaPubli, nomPeriodico);
+
+
+                    
+
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 0:
+                    Console.WriteLine("Bye, Bye");
+                    break;
+
+                default:
+                    Console.WriteLine("Parámetro no válido");
+                    break;
+            }
+        } while (opc != 0);
+
+
+
+
+
+
+    }
+}
---
+++ Punto4/Punto4/Publicaciones.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Punto4
+{
+    public class Publicaciones
+    {
+        protected int NumHojas, ValorHoja, ValorHojaColor;
+        protected string Titulo, FechaPubli;
+
+
+
+        public Publicaciones(int numHojas, int valorHoja, int valorHojaColor, string titulo, string fechaPubli)
+        {
+            NumHojas = numHojas;
+            ValorHoja = valorHoja;
+            ValorHojaColor = valorHojaColor;
+            Titulo = titulo;
+            FechaPubli = fechaPubli;
+        }
+    }
+
+    class Periodico : Publicaciones
+    {
+        public string NomPeriodico { get; set; }
+
+        public Periodico(int numHojas, int valorHoja, int valorHojaColor, string titulo, string fechaPubli, string nomPeriodico)
+            : base(numHojas, valorHoja, valorHojaColor, titulo, fechaPubli)
+        {
+            NomPeriodico = nomPeriodico;
+        }
+    }
+
+    class Libro : Publicaciones
+    {
+        public string NomAutor { get; set; }
+        public string TipoPortada { get; set; }
+
+        public Libro(int numHojas, int valorHoja, int valorHojaColor, string titulo, string fechaPubli, string nomAutor, string tipoPortada)
+            : base(numHojas, valorHoja, valorHojaColor, titulo, fechaPubli)
+        {
+            NomAutor = nomAutor;
+            TipoPortada = tipoPortada;
+        }
+    }
+}
